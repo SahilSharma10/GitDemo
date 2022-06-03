b0ef92ae8e0490f148366a66fd61d0e19881487b
sahilsharma10 <sahil.shady07@gmail.com>
2022-06-03 15:54:41 +0530

    first commit for API

---
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -0,0 +1,21 @@
+package cucumber.Options;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features={"src/test/java/features"},
+plugin={"pretty",
+		"html:target/cucumber-reports/cucumber.html",
+        "json:target/cucumber-reports/cucumber.json"},
+glue={"stepDefinations"})
+
+public class TestRunner {
+	
+	
+	//tags= "@DeletePlace"	
+
+}
